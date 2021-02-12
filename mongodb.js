8088569cf463bf6aa22804af85cb962c017a89fe
Rohith <rohithv355@gmail.com>
2021-02-12 18:38:14 +0530

    Array of tables commit

--- mongodb.js
+++ mongodb.js
@@ -9,20 +9,27 @@ const checkString = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDBhOTQ0ND
 //List of documents to be returned
 var documentList = [];
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true , useUnifiedTopology: true}, (error, client) => {
-    if(error) {
-        return console.log('Unable to connect to database!')
-    }
+//List of collections checked
+const collectionName = ['users']
 
-    const db = client.db(databaseName) 
-    var cursor = db.collection('users').find();
-    
-    // Execute the each command, triggers for each document
-    cursor.each(function(err, item) {
-        let documentString = JSON.stringify(item)
-        if(documentString.includes(checkString)) {
-            console.log(item._id)
-            documentList.push(item._id)
+collectionName.forEach((name) => {
+    MongoClient.connect(connectionURL, { useNewUrlParser: true , useUnifiedTopology: true}, (error, client) => {
+        if(error) {
+            return console.log('Unable to connect to database!')
         }
-    });
-})
+    
+        const db = client.db(databaseName) 
+        var cursor = db.collection(name).find();
+        
+        // Execute the each command, triggers for each document
+        cursor.each(function(err, item) {
+            let documentString = JSON.stringify(item)
+            if(documentString.includes(checkString)) {
+                console.log(item._id)
+                documentList.push(item)
+            }
+        });
+    })
+  });
+
+return documentList
